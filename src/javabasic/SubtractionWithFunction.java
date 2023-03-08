bf405f6596c4763bb39d6a77acfc97af68b09c22
nikita <nikita.motwani@theporter.in>
2023-03-08 10:52:23 +0530

    Substraction with function Program

---
+++ src/javabasic/SubtractionWithFunction.java
@@ -0,0 +1,20 @@
+package javabasic;
+
+public class SubtractionWithFunction {
+
+	public static void main(String[] args) {
+
+		int a=10;
+		int b=20;
+		
+		int result= substraction(a,b);
+			
+		System.out.println(result);
+	}
+
+	public static int substraction(int a, int b) {
+		return (a-b);
+
+	}
+
+}
