09bae4af5dce46f1b1702094ada69bad1365e915
nikita <nikita.motwani@theporter.in>
2023-03-08 10:22:11 +0530

    Substraction Program committed

---
+++ src/javabasic/Substraction.java
@@ -0,0 +1,16 @@
+package javabasic;
+
+public class Substraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10;
+		int num2 = 20;
+		
+		int result=(num2-num1);
+		
+		System.out.println("The result is: " + result);
+
+	}
+
+}
